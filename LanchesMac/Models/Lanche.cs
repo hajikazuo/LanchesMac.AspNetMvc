3bf00b0c91a504747b0caf9dc721302293ddb9ea
hajikazuo <hajikazuo@outlook.com>
2024-03-11 17:59:44 -0300

    Add project files.

---
+++ LanchesMac/Models/Lanche.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.Models;
+
+public class Lanche
+{
+    public int LancheId { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} deve ser informado")]
+    [Display(Name = "Nome do Lanche")]
+    [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "A {0} deve ser informada")]
+    [Display(Name = "Descrição do Lanche")]
+    [StringLength(200, MinimumLength = 20, ErrorMessage = "{0} deve ter entre {2} e {1} caracteres")]
+    public string DescricaoCurta { get; set; }
+
+    [Required(ErrorMessage = "O {0} do lanche deve ser informada")]
+    [Display(Name = "Descrição detalhada do Lanche")]
+    [StringLength(200, MinimumLength = 20, ErrorMessage = "{0} deve ter entre {2} e {1} caracteres")]
+    public string DescricaoDetalhada { get; set; }
+
+    [Required(ErrorMessage = "Informe o {0} do lanche")]
+    [Display(Name = "Preço")]
+    [Column(TypeName = "decimal(10,2)")]
+    [Range(1, 999.99, ErrorMessage = "O {0} deve estar entre {1} e {2}")]
+    public decimal Preco { get; set; }
+
+    [Display(Name = "Caminho Imagem Normal")]
+    [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
+    public string ImagemUrl { get; set; }
+
+    [Display(Name = "Caminho Imagem Miniatura")]
+    [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
+    public string ImagemThumbnailUrl { get; set; }
+
+    [Display(Name = "Preferido?")]
+    public bool IsLanchePreferido { get; set; }
+
+    [Display(Name = "Estoque")]
+    public bool EmEstoque { get; set; }
+
+    public int CategoriaId { get; set; }
+    public virtual Categoria? Categoria { get; set; }
+
+}
